4b754710696db4982f1c1e55cfb2ff3bffe287b6
Boston Dell-Vandenberg <boston@pomelodesign.com>
2014-08-03 20:46:54 -0400

    refactor some js

--- js/upw-admin.js
+++ js/upw-admin.js
@@ -2,10 +2,12 @@ jQuery(document).ready(function($) {
 
   $('.widget').on('click', '.upw-tab-item', function(event) {
     event.preventDefault();
-    $(this).parents('.widget-inside').find('.upw-tab-item').removeClass('active');
+    var widget = $(this).parents('.widget');
+    console.log(widget);
+    widget.find('.upw-tab-item').removeClass('active');
     $(this).addClass('active');
-    $(this).parents('.widget-inside').find('.upw-tab').addClass('upw-hide');
-    $(this).parents('.widget-inside').find('.' + $(this).data('toggle')).removeClass('upw-hide');
+    widget.find('.upw-tab').addClass('upw-hide');
+    widget.find('.' + $(this).data('toggle')).removeClass('upw-hide');
   });
 
 });
